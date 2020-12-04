9b1184e2424cba0527661a8cadb3767dd0a301ea
Eana Hufwe <ilove@1a23.com>
2020-12-04 15:04:36 +0800

    minify HTML

--- render_data.py
+++ render_data.py
@@ -1,6 +1,7 @@
 from jinja2 import Template
 import json
 from datetime import datetime, timezone
+import htmlmin
 
 with open("template.html", "r") as f:
     template_content = f.read()
@@ -9,6 +10,7 @@ with open("data.json", "r") as f:
 
 template = Template(template_content)
 output = template.render(messages=data, time=datetime.now(tz=timezone.utc).isoformat())
+output = htmlmin.minify(output)
 
 with open("index.html", "w") as f:
     f.write(output)
